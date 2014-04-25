ec3ea3a8088f92323d500c5d05b827e7cd8a3f84
Paul van Assen <source@paules.nl>
2014-04-25 14:17:30 +0200

    Added plot color for gradients

---
+++ src/main/java/nl/pvanassen/highchart/api/plotoption/PlotStop.java
@@ -0,0 +1,23 @@
+package nl.pvanassen.highchart.api.plotoption;
+
+import java.awt.Color;
+
+public class PlotStop {
+    private final int pos;
+    private final Color color;
+
+    PlotStop(int pos, Color color) {
+        super();
+        this.pos = pos;
+        this.color = color;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+}
