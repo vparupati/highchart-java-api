0c1dc005096c6308a6f5a9aa505ef20793c3ecf5
Paul van Assen <source@paules.nl>
2014-07-10 20:22:13 +0200

    Fixed Double notations in JSON

--- src/main/java/nl/pvanassen/highchart/api/base/GsonHelper.java
+++ src/main/java/nl/pvanassen/highchart/api/base/GsonHelper.java
@@ -1,43 +1,52 @@
 package nl.pvanassen.highchart.api.base;
 
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+
 import nl.pvanassen.highchart.api.format.DateTimeLabelFormats;
 import nl.pvanassen.highchart.api.serializer.DateTimeLabelFormatsSerializer;
 import nl.pvanassen.highchart.api.serializer.StyleSerializer;
 
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 
 public final class GsonHelper {
 
-    private static final String     DATE_FORMAT  = "yyyyMMdd";
+    private static final String DATE_FORMAT = "yyyyMMdd";
 
-    private static final String     USER_OBJECT = "userObject";
+    private static final String USER_OBJECT = "userObject";
 
-    private static final GsonHelper INSTANCE    = new GsonHelper();
+    private static final GsonHelper INSTANCE = new GsonHelper();
 
-    static String toJson( Object object ) {
-        return INSTANCE.gsonBuilder.create().toJson( object );
+    static String toJson(Object object) {
+        return INSTANCE.gsonBuilder.create().toJson(object);
     }
 
     private final GsonBuilder gsonBuilder;
 
     private GsonHelper() {
-        gsonBuilder = new GsonBuilder().registerTypeAdapter( DateTimeLabelFormats.class, new DateTimeLabelFormatsSerializer() ) //
-        .registerTypeAdapter( Style.class, new StyleSerializer() )//
-        .setDateFormat( DATE_FORMAT )//
-        .setExclusionStrategies( new ExclusionStrategy() {
+        gsonBuilder = new GsonBuilder()
+                .registerTypeAdapter(DateTimeLabelFormats.class, new DateTimeLabelFormatsSerializer()) //
+                .registerTypeAdapter(Style.class, new StyleSerializer())//
+                .setDateFormat(DATE_FORMAT)//
+                .setExclusionStrategies(new ExclusionStrategy() {
 
-            @Override
-            public boolean shouldSkipClass( Class<?> arg0 ) {
-                return false;
-            }
+                    @Override
+                    public boolean shouldSkipClass(Class<?> arg0) {
+                        return false;
+                    }
 
-            @Override
-            public boolean shouldSkipField( FieldAttributes attributes ) {
-                return attributes.getName().equals( USER_OBJECT );
-            }
+                    @Override
+                    public boolean shouldSkipField(FieldAttributes attributes) {
+                        return attributes.getName().equals(USER_OBJECT);
+                    }
 
-        } );
+                }).registerTypeAdapter(Double.class,  new JsonSerializer<Double>() {
+                    @Override
+                    public JsonElement serialize(Double originalValue, Type typeOf, JsonSerializationContext context) {
+                        BigDecimal bigValue = BigDecimal.valueOf(originalValue);
+
+                        return new JsonPrimitive(bigValue.toPlainString());
+                    }
+                });
     }
 }
