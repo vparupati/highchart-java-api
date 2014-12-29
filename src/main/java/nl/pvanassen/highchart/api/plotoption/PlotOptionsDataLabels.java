cea4d332f8d3ca3807940c22b1f7fba4b4688da7
Shaun <s.lefeuvre@gmail.com>
2014-12-29 16:12:08 -0600

    Version 4.0.4 compatability.

--- src/main/java/nl/pvanassen/highchart/api/plotoption/PlotOptionsDataLabels.java
+++ src/main/java/nl/pvanassen/highchart/api/plotoption/PlotOptionsDataLabels.java
@@ -1,91 +1,399 @@
 package nl.pvanassen.highchart.api.plotoption;
 
 import nl.pvanassen.highchart.api.base.BaseObject;
+import nl.pvanassen.highchart.api.base.Style;
 
 public class PlotOptionsDataLabels extends BaseObject {
 
-    private String color;
+    private String align = null;
+    
+    private String backgroundColor = null;
+            
+    private String borderColor = null;
+    
+    private Integer borderRadius = null;
+    
+    private Integer borderWidth = null;
+    
+    private String color = null;
+    
+    private Boolean crop = null;
+    
+    private Boolean defer = null;
 
-    private boolean enabled;
+    private Boolean enabled = null;
+    
+    private String format = null;
 
-    private Object formatter;
+    private Object formatter = null;
+    
+    private Boolean inside = null;
+    
+    private String overflow = null;
+    
+    private Integer padding = null;
+            
+    private Double rotation = null;
+    
+    private Boolean shadow = null;
+    
+    private Style style = null;
+    
+    private Boolean useHTML = null;
+    
+    private String verticalAlign = null;
+    
+    private Integer x = null;
+    
+    private Integer y = null;
+    
+    private Integer zIndex = null;
 
-    private Integer x, y, distance;
-
-    private String align = "center";
-
-    private double rotation;
-
+    /**
+     * @return the align
+     */
     public String getAlign() {
         return align;
     }
 
-    public String getColor() {
-        return color;
-    }
-
-    public int getDistance() {
-        return distance;
-    }
-
-    public Object getFormatter() {
-        return formatter;
-    }
-
-    public double getRotation() {
-        return rotation;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
+    /**
+     * @param align the align to set
+     * @return 
+     */
     public PlotOptionsDataLabels setAlign(String align) {
         this.align = align;
         return this;
     }
 
+    /**
+     * @return the backgroundColor
+     */
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    /**
+     * @param backgroundColor the backgroundColor to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+        return this;
+    }
+
+    /**
+     * @return the borderColor
+     */
+    public String getBorderColor() {
+        return borderColor;
+    }
+
+    /**
+     * @param borderColor the borderColor to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setBorderColor(String borderColor) {
+        this.borderColor = borderColor;
+        return this;
+    }
+
+    /**
+     * @return the borderRadius
+     */
+    public Integer getBorderRadius() {
+        return borderRadius;
+    }
+
+    /**
+     * @param borderRadius the borderRadius to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setBorderRadius(Integer borderRadius) {
+        this.borderRadius = borderRadius;
+        return this;
+    }
+
+    /**
+     * @return the borderWidth
+     */
+    public Integer getBorderWidth() {
+        return borderWidth;
+    }
+
+    /**
+     * @param borderWidth the borderWidth to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setBorderWidth(Integer borderWidth) {
+        this.borderWidth = borderWidth;
+        return this;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     * @return 
+     */
     public PlotOptionsDataLabels setColor(String color) {
         this.color = color;
         return this;
     }
 
-    public PlotOptionsDataLabels setDistance(int distance) {
-        this.distance = distance;
+    /**
+     * @return the crop
+     */
+    public Boolean getCrop() {
+        return crop;
+    }
+
+    /**
+     * @param crop the crop to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setCrop(Boolean crop) {
+        this.crop = crop;
         return this;
     }
 
-    public PlotOptionsDataLabels setEnabled(boolean enabled) {
+    /**
+     * @return the defer
+     */
+    public Boolean getDefer() {
+        return defer;
+    }
+
+    /**
+     * @param defer the defer to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setDefer(Boolean defer) {
+        this.defer = defer;
+        return this;
+    }
+
+    /**
+     * @return the enabled
+     */
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    /**
+     * @param enabled the enabled to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setEnabled(Boolean enabled) {
         this.enabled = enabled;
         return this;
     }
 
+    /**
+     * @return the format
+     */
+    public String getFormat() {
+        return format;
+    }
+
+    /**
+     * @param format the format to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setFormat(String format) {
+        this.format = format;
+        return this;
+    }
+
+    /**
+     * @return the formatter
+     */
+    public Object getFormatter() {
+        return formatter;
+    }
+
+    /**
+     * @param formatter the formatter to set
+     * @return 
+     */
     public PlotOptionsDataLabels setFormatter(Object formatter) {
         this.formatter = formatter;
         return this;
     }
 
-    public PlotOptionsDataLabels setRotation(double rotation) {
+    /**
+     * @return the inside
+     */
+    public Boolean getInside() {
+        return inside;
+    }
+
+    /**
+     * @param inside the inside to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setInside(Boolean inside) {
+        this.inside = inside;
+        return this;
+    }
+
+    /**
+     * @return the overflow
+     */
+    public String getOverflow() {
+        return overflow;
+    }
+
+    /**
+     * @param overflow the overflow to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setOverflow(String overflow) {
+        this.overflow = overflow;
+        return this;
+    }
+
+    /**
+     * @return the padding
+     */
+    public Integer getPadding() {
+        return padding;
+    }
+
+    /**
+     * @param padding the padding to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setPadding(Integer padding) {
+        this.padding = padding;
+        return this;
+    }
+
+    /**
+     * @return the rotation
+     */
+    public Double getRotation() {
+        return rotation;
+    }
+
+    /**
+     * @param rotation the rotation to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setRotation(Double rotation) {
         this.rotation = rotation;
         return this;
     }
 
-    public PlotOptionsDataLabels setX(int x) {
+    /**
+     * @return the shadow
+     */
+    public Boolean getShadow() {
+        return shadow;
+    }
+
+    /**
+     * @param shadow the shadow to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setShadow(Boolean shadow) {
+        this.shadow = shadow;
+        return this;
+    }
+
+    /**
+     * @return the style
+     */
+    public Style getStyle() {
+        if(this.style == null) {
+            this.style = new Style();
+        }
+        return style;
+    }
+
+    /**
+     * @return the useHTML
+     */
+    public Boolean getUseHTML() {
+        return useHTML;
+    }
+
+    /**
+     * @param useHTML the useHTML to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setUseHTML(Boolean useHTML) {
+        this.useHTML = useHTML;
+        return this;
+    }
+
+    /**
+     * @return the verticalAlign
+     */
+    public String getVerticalAlign() {
+        return verticalAlign;
+    }
+
+    /**
+     * @param verticalAlign the verticalAlign to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setVerticalAlign(String verticalAlign) {
+        this.verticalAlign = verticalAlign;
+        return this;
+    }
+
+    /**
+     * @return the x
+     */
+    public Integer getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setX(Integer x) {
         this.x = x;
         return this;
     }
 
-    public PlotOptionsDataLabels setY(int y) {
+    /**
+     * @return the y
+     */
+    public Integer getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setY(Integer y) {
         this.y = y;
         return this;
     }
+
+    /**
+     * @return the zIndex
+     */
+    public Integer getzIndex() {
+        return zIndex;
+    }
+
+    /**
+     * @param zIndex the zIndex to set
+     * @return 
+     */
+    public PlotOptionsDataLabels setzIndex(Integer zIndex) {
+        this.zIndex = zIndex;
+        return this;
+    }
+
 
 }
--- src/main/java/nl/pvanassen/highchart/api/plotoption/PlotOptionsMarker.java
+++ src/main/java/nl/pvanassen/highchart/api/plotoption/PlotOptionsMarker.java
@@ -4,11 +4,19 @@ import nl.pvanassen.highchart.api.base.BaseObject;
 
 public class PlotOptionsMarker extends BaseObject {
 
-    private String symbol;
-
-    private boolean enabled;
-
+    private Boolean enabled;
+    
+    private String  fillColor;
+    
+    private String lineColor;
+    
+    private Integer lineWidth;
+    
+    private Float radius;
+    
     private PlotMarkerStates states;
+
+    private String symbol;
 
     public PlotOptionsMarker() {
     }
@@ -20,19 +28,119 @@ public class PlotOptionsMarker extends BaseObject {
         return states;
     }
 
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public boolean isEnabled() {
+    /**
+     * Enable or disable the point marker. Defaults to true.
+     * @return the enabled
+     */
+    public Boolean getEnabled() {
         return enabled;
     }
 
-    public PlotOptionsMarker setEnabled(boolean enabled) {
+    /**
+     * Enable or disable the point marker. Defaults to true.
+     * @param enabled the enabled to set
+     * @return 
+     */
+    public PlotOptionsMarker setEnabled(Boolean enabled) {
         this.enabled = enabled;
         return this;
     }
 
+    /**
+     * The fill color of the point marker. When null, the series' 
+     * or point's color is used.
+     * @return the fillColor
+     */
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    /**
+     * The fill color of the point marker. When null, the series' 
+     * or point's color is used.
+     * @param fillColor the fillColor to set
+     * @return 
+     */
+    public PlotOptionsMarker setFillColor(String fillColor) {
+        this.fillColor = fillColor;
+        return this;
+    }
+
+    /**
+     * The color of the point marker's outline. When null, the series' 
+     * or point's color is used. Defaults to #FFFFFF.
+     * @return the lineColor
+     */
+    public String getLineColor() {
+        return lineColor;
+    }
+
+    /**
+     * The color of the point marker's outline. When null, the series' 
+     * or point's color is used. Defaults to #FFFFFF.
+     * @param lineColor the lineColor to set
+     * @return 
+     */
+    public PlotOptionsMarker setLineColor(String lineColor) {
+        this.lineColor = lineColor;
+        return this;
+    }
+
+    /**
+     * The width of the point marker's outline. Defaults to 0.
+     * @return the lineWidth
+     */
+    public Integer getLineWidth() {
+        return lineWidth;
+    }
+
+    /**
+     * The width of the point marker's outline. Defaults to 0.
+     * @param lineWidth the lineWidth to set
+     * @return 
+     */
+    public PlotOptionsMarker setLineWidth(Integer lineWidth) {
+        this.lineWidth = lineWidth;
+        return this;
+    }
+
+    /**
+     * The radius of the point marker. Defaults to 4.
+     * @return the radius
+     */
+    public Float getRadius() {
+        return radius;
+    }
+
+    /**
+     * The radius of the point marker. Defaults to 4.
+     * @param radius the radius to set
+     * @return 
+     */
+    public PlotOptionsMarker setRadius(Float radius) {
+        this.radius = radius;
+        return this;
+    }
+
+    /**
+     * A predefined shape or symbol for the marker. When null, the symbol is 
+     * pulled from options.symbols. Other possible values are "circle", 
+     * "square", "diamond", "triangle" and "triangle-down".Additionally, 
+     * the URL to a graphic can be given on this form: "url(graphic.png)".
+     * @return the symbol
+     */
+    public String getSymbol() {
+        return symbol;
+    }
+
+    /**
+     * A predefined shape or symbol for the marker. When null, the symbol is 
+     * pulled from options.symbols. Other possible values are "circle", 
+     * "square", "diamond", "triangle" and "triangle-down".Additionally, 
+     * the URL to a graphic can be given on this form: "url(graphic.png)".
+     * @param symbol the symbol to set
+     * @return 
+     */
     public PlotOptionsMarker setSymbol(String symbol) {
         this.symbol = symbol;
         return this;
